92eca121d73d0f622da75aff78ab54a9b62f4474
Ahmed Badr Shabaan Ahmed <59667579+AhmedBadrShaban@users.noreply.github.com>
2023-12-25 22:47:07 +0200

    ..nav bar

--- src/app/modules/conteributer/conteributer.module.ts
+++ src/app/modules/conteributer/conteributer.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from './../../shared/shared.module';
 import { ConteributerRoutingModule } from './conteributer-routing.module';
 import { ConteributerComponent } from './conteributer.component';
+import { AdminNavBarComponent } from '../admin/components/admin-nav-bar/admin-nav-bar.component';
 
 
 @NgModule({
   declarations: [
     ConteributerComponent
+    
   ],
   imports: [
     CommonModule,
     ConteributerRoutingModule,
-    SharedModule
+    SharedModule,
+    AdminNavBarComponent
   ]
 })
 export class ConteributerModule { }
