1ad6dfd9b9677e0014b927f6af99684b01f98f4a
AhmedBadrShaban <ahmedbadrr417@gmail.com>
2026-06-27 04:18:03 +0300

    refactor: clean up imports and formatting in AdminModule

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './../../shared/shared.module';
-import {RouterLink} from "@angular/router";
-import {NzTableModule} from "ng-zorro-antd/table";
+import { RouterLink } from "@angular/router";
+import { NzTableModule } from "ng-zorro-antd/table";
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { DoctorComponent } from './components/doctors/doctor/doctor.component';
@@ -13,15 +13,13 @@ import { ReceptionistsComponent } from './components/receptionists/receptionists
 import { ReceptionistProfileComponent } from './components/receptionists/receptionist/receptionist.component';
 import { ServicesComponent } from './components/services/services.component';
 import { PackagesComponent } from './components/packages/packages.component';
- import {NzBadgeModule} from "ng-zorro-antd/badge";
-import {NzDropDownModule} from "ng-zorro-antd/dropdown";
+import { NzBadgeModule } from "ng-zorro-antd/badge";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
 import { AddNewReceptionistComponent } from './components/receptionists/add-new-receptionist/add-new-receptionist.component';
 import { AddNewPackageComponent } from './components/packages/add-new-package/add-new-package.component';
 import { ConfirmDialogComponent } from './components/packages/confirm-dialog/confirm-dialog.component';
 import { AddNewServiceComponent } from './components/services/add-new-service/add-new-service.component';
-// import { MaterialsComponent } from './components/materials/materials.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
-// import {AutoCompleteComponent} from "../shared/components/auto-complete/auto-complete.component";
 import { AdminProfileComponent } from './components/admin-profile/admin-profile.component';
 import { MaterialsComponent } from './components/materials/materials.component';
 import { AddNewMatrialComponent } from './components/materials/add-new-matrial/add-new-matrial.component';
@@ -29,12 +27,10 @@ import { AdminNavBarComponent } from './components/admin-nav-bar/admin-nav-bar.c
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { ReceptionistModule } from '../receptionist/receptionist.module';
- import { ContributorsComponent } from './components/contributors/contributors.component';
+import { ContributorsComponent } from './components/contributors/contributors.component';
 import { AddContributorComponent } from './components/contributors/add-contributor/add-contributor.component';
-
-import {NzTabsModule} from "ng-zorro-antd/tabs";
- import { MonthlyMoneyReportComponent } from './components/monthly-income/income.component';
-
+import { NzTabsModule } from "ng-zorro-antd/tabs";
+import { MonthlyMoneyReportComponent } from './components/monthly-income/income.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -48,66 +44,61 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { DebitReportComponent } from './components/reports/depit-report/debit-report.component';
- import { DebitMovementsComponent } from './components/reports/debit-movements/debit-movements.component';
-  
+import { DebitMovementsComponent } from './components/reports/debit-movements/debit-movements.component';
+ import { ReportsComponent } from '../../shared/components/reports/reports.component';
 
 @NgModule({
   declarations: [
-      AdminComponent,
-      DoctorsComponent,
-      DoctorComponent,
-      AddNewDoctorComponent,
-      ReceptionistsComponent,
-      ReceptionistProfileComponent,
-      ServicesComponent,
-      PackagesComponent,
-      ConfirmDialogComponent,
-        MonthlyMoneyReportComponent,
-      AddNewReceptionistComponent,
-      AddNewPackageComponent,
-      AddNewServiceComponent,
-      ReservationsComponent,
-      AdminProfileComponent,
-      MaterialsComponent,
-      AddNewMatrialComponent,
-        ContributorsComponent,
-      AddContributorComponent,
+    AdminComponent,
+    DoctorsComponent,
+    DoctorComponent,
+    AddNewDoctorComponent,
+    ReceptionistsComponent,
+    ReceptionistProfileComponent,
+    ServicesComponent,
+    PackagesComponent,
+    ConfirmDialogComponent,
+    MonthlyMoneyReportComponent,
+    AddNewReceptionistComponent,
+    AddNewPackageComponent,
+    AddNewServiceComponent,
+    ReservationsComponent,
+    AdminProfileComponent,
+    MaterialsComponent,
+    AddNewMatrialComponent,
+    ContributorsComponent,
+    AddContributorComponent,
     DebitReportComponent,
     DebitMovementsComponent
-    
-    ],
+  ],
 
-    imports: [
-        CommonModule,
-        AdminRoutingModule,
-        AdminNavBarComponent,
-        SharedModule,
-        ReactiveFormsModule,
-        FormsModule,
-        RouterLink,
-        NzTableModule,
-        NzBadgeModule,
-        NzDropDownModule,
-        NzAutocompleteModule,
-        ReceptionistModule,
-      CommonModule,
-      FormsModule,
-         NzTabsModule,
-        MatCardModule,
-        MatButtonModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatInputModule,
-        MatProgressSpinnerModule,
-        MatSnackBarModule,
-        MatTableModule,
-        MatChipsModule,
-        MatTooltipModule,
-      AdminSidebarComponent,
-
-       
-       
-    ]
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    AdminNavBarComponent,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterLink,
+    NzTableModule,
+    NzBadgeModule,
+    NzDropDownModule,
+    NzAutocompleteModule,
+    ReceptionistModule,
+    NzTabsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatChipsModule,
+    MatTooltipModule,
+    AdminSidebarComponent,
+     ReportsComponent,
+  ]
 })
 export class AdminModule { }
